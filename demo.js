aa7ccddcba27bc0abdd9f9e3be9433012a3f8f93
rajeshpillai <pillai.rajesh@gmail.com>
2024-05-03 21:10:50 +0530

    TODO: Render custom function. NOTE:  Destructing needed to avoid printing function returning text as individual characters.  TROUBLESHOOT MORE

--- demo.js
+++ demo.js
@@ -1,30 +1,35 @@
 /** @jsx TinyReact.createElement */
 
-/*** Step 1,2,3,4 - createElement */
-
 const root = document.getElementById("root");
 
+let Title = function (props) {
+  return <h1>{props.children}</h1>
+}
 
-// Step 5, 6
+// let Step2 = (
+//   <div>
+//     <h1 className="header">Hello Tiny React!</h1>
+//     <h2>(coding nirvana)</h2>
+//     <div>nested 1<div>nested 1.1</div></div>
+//     <h3 style="background-color:yellow">(OBSERVE: I said it!!)</h3>
+//     {2 == 1 && <div>Render this if 2==1</div>}
+//     {2 == 2 && <div>{2}</div>}
+//     <span>Something goes here...</span>
+//     <button onClick={() => alert("This has changed!")}>Click me!</button>
+//   </div>
+// );
+
+// let Step2 = (
+//     <Title class="header">Tiny React!</Title>
+// )
 
 let Step2 = (
-  <div>
-    <h1 className="header">Hello Tiny React!</h1>
-    <h2>(coding nirvana)</h2>
-    <div>nested 1<div>nested 1.1</div></div>
-    <h3 style="background-color:yellow">(OBSERVE: I said it!!)</h3>
-    {2 == 1 && <div>Render this if 2==1</div>}
-    {2 == 2 && <div>{2}</div>}
-    <span>Something goes here...</span>
-    <button onClick={() => alert("This has changed!")}>Click me!</button>
+  <div class="container">
+    <Title class="header">Tiny React!</Title>
+    <h2>It Rocks!</h2>
   </div>
 );
 
-Step2 = (
-  <div class="container">
-    <h2>Hello Tiny React!</h2>
-  </div>
-);
 console.log(Step2);
 
 TinyReact.render(Step2, root);
